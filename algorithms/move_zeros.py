02c74d0b7018d42e533c82965d10729b7a644661
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2019-12-10 18:20:17 +0500

    Create move_zeros.py

---
+++ algorithms/move_zeros.py
@@ -0,0 +1,8 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        number_of_zeros = 0
+
+        for index in range(len(nums)-1, -1, -1):
+            if nums[index] == 0:
+                del nums[index]
+                nums.append(0)
