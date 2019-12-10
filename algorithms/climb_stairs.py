a971bb4f3d30a8d2ba847d617f42a806c9393823
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2019-12-10 18:29:25 +0500

    add Climb Stairs problem

---
+++ algorithms/climb_stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        "Fibonacci"
+        a, b = 1, 1
+        
+        i = 1
+        while i < n:
+            i += 1
+            a = a + b
+            b = a - b
+
+        return a
