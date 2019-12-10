ef443eb808e0fe3a5f8edc40a399cd3607d481f7
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2019-12-10 18:21:49 +0500

    add Two Sum problem

---
+++ algorithms/two_sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers, target):
+        complements = {}
+        
+        for index, number in enumerate(numbers):
+            complements[target-number] = index
+            
+        for index, number in enumerate(numbers):
+            if number in complements and complements[number] != index:
+                
+                return [index, complements[number]]
