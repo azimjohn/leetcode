7bda123a5c5cf9ba8c2878c1c36c63fd2231b871
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2019-12-14 18:59:23 +0500

    add Pascal's Triangle

---
+++ algorithms/pascals_triangle.py
@@ -0,0 +1,9 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        rows = [[1 for j in range(i)] for i in range(1, numRows+1)]
+ 
+        for i in range(2, numRows):
+            for j in range(1, i):
+                rows[i][j] = rows[i-1][j-1] + rows[i-1][j]
+
+        return rows
