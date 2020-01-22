bcec84d4121d6a96ef8af44ce1c8e8eb359c4896
AzimjonDev <gmail@azimjon.com>
2020-01-22 16:57:27 +0500

    [golang] Symmetric Tree

---
+++ top_interview_questions/1_easy/4_trees/symmetric_tree/solution.go
@@ -0,0 +1,27 @@
+package main
+
+func main() {
+
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func isSymmetric(root *TreeNode) bool {
+	return helper(root, root)
+}
+
+func helper(left, right *TreeNode) bool {
+	if left == nil && right == nil {
+		return true
+	}
+
+	if left == nil || right == nil {
+		return false
+	}
+
+	return left.Val == right.Val && helper(left.Left, right.Right) && helper(left.Right, right.Left)
+}
