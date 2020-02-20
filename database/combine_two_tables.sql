9a8d259eca15ea5a17451af5240a5fdff2156e01
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2020-02-20 15:48:52 +0100

    Create combine_two_tables.sql

---
+++ database/combine_two_tables.sql
@@ -0,0 +1,4 @@
+-- https://leetcode.com/problems/combine-two-tables/submissions/
+SELECT Person.FirstName, Person.LastName, Address.City, Address.State
+FROM Person
+LEFT JOIN Address ON Person.PersonId = Address.PersonId
