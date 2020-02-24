0dc87317237fe8210a4371e8b31c5906b1e4d662
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2020-02-24 16:44:36 +0100

    Update k_closest_points_to_origin.py

--- algorithms/k_closest_points_to_origin.py
+++ algorithms/k_closest_points_to_origin.py
@@ -3,12 +3,7 @@ import heapq
 
 class Solution:
     def kClosest(self, points: List[List[int]], K: int) -> List[List[int]]:
-        points_by_distance_squared = []
-        
-        for point in points:
-            distance_squared = point[0] ** 2 + point[1] ** 2
-            points_by_distance_squared.append((distance_squared, point))
-        
-        k_closest = heapq.nsmallest(K, points_by_distance_squared)
-        
-        return [element[1] for element in k_closest]
+        def distance_squared(point):
+            return point[0] ** 2 + point[1] ** 2
+
+        return heapq.nsmallest(K, points, key=distance_squared)
