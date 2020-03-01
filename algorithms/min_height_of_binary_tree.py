545acdf0b86306b300b10717cb3408ea39331c00
AzimjonDev <gmail@azimjon.com>
2020-03-01 22:30:30 +0500

    add min height of tree

---
+++ algorithms/min_height_of_binary_tree.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+
+        if not (root.left or root.right):
+            return 1
+
+        if not root.right:
+            return self.minDepth(root.left) + 1
+
+        if not root.left:
+            return self.minDepth(root.right) + 1
+
+        return min([
+            self.minDepth(root.left) + 1,
+            self.minDepth(root.right) + 1,
+        ])
