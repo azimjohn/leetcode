0031c124e0071cb25c8a3a5a4ba7dcbae884246a
Azimjon Pulatov <35038240+azimjohn@users.noreply.github.com>
2020-07-30 15:58:39 +0200

    Update "Number of 1bits" with trick from Kernighan

--- top_interview_questions/1_easy/9_others/number_of_1_bits/solution.py
+++ top_interview_questions/1_easy/9_others/number_of_1_bits/solution.py
@@ -1,4 +1,9 @@
-# TODO: use bit hacks
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        return bin(n).count('1')
+        count = 0
+        
+        while n != 0:
+            n = n & (n - 1)
+            count += 1
+        
+        return count
